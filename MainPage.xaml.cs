7c6d1abc4a3d79eb68301b34aec9c22c5f0fd8c9
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2014-12-02 14:09:08 +0530

    created main page

---
+++ MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//My imports
+using System.Diagnostics;
+using Windows.ApplicationModel.Resources;
+using Windows.Web.Http;
+using Windows.Web.Http.Filters;
+using Windows.Storage;
+using System.Threading;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace Pocket_Client
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+      private readonly ResourceLoader resourceLoader = ResourceLoader.GetForCurrentView("Resources");
+      private ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+      private CancellationTokenSource cts;
+        public MainPage()
+        {
+          
+            this.InitializeComponent();
+          
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+          String request_token = localSettings.Values["request_token"] as String;
+          System.Diagnostics.Debug.WriteLine(request_token);
+          if (request_token == null)
+          {
+           
+          }
+           
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+    }
+}
---
+++ MainPageHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//My Usings
+using Windows.UI.Xaml.Controls;
+using Windows.Web.Http;
+
+namespace Pocket_Client
+{
+  public partial class MainPage : Page
+  {
+
+    private async void generateRequestToken()
+    {
+        String url = resourceLoader.GetString("requestTokenURI");
+        HttpClient request = new HttpClient();
+        Uri uri = new Uri(url);
+        Dictionary<String, String> postValue = new Dictionary<string, string>();
+        postValue.Add("consumer_key", localSettings.Values["consumer_key"] as String);
+        HttpResponseMessage response = await request.PostAsync(uri, new HttpStringContent("")).AsTask(cts.Token);
+    }
+  }
+}
